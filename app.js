76c85418bfb7b82843c3ffc4a107f06733c33e3e
usama-afzal7 <afzalusama77@gmail.com>
2023-11-20 19:03:25 +0500

    Initial COmmit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req);
+const express = require('express');
+
+const app = express();
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<h1>The "Add Product" Page</h1>'); 
+});
+app.use('/',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Expess JS</h1>');
 });
 
-server.listen(3000);
+app.listen(3000);
